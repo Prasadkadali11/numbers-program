b1a0a03539819a4cf5881aec19694f7ff7500c42
Kadali Durga Prasad <161697177+Prasadkadali11@users.noreply.github.com>
2024-07-23 16:34:40 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,21 @@
+package numbersprogramming;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort {
+	    public static void main(String[] args) {
+	        List<Integer> numbers = new ArrayList<Integer>();
+	        numbers.add(3);
+	        numbers.add(1);
+	        numbers.add(4);
+	        numbers.add(2);
+	        
+	        Collections.sort(numbers);
+	        
+	        System.out.println("Sorted List: " + numbers);
+	    }
+	}
+
+
